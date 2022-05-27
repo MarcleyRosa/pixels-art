3a21ad9fa66ebb492c3f44d8bdcdd1a20be0c99f
Marcley <marcleyrosa@gmail.com>
2022-05-27 18:37:11 -0300

    Atualização dos requisitos 2 a 6

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const criarH1 = document.createElement('h1');
-criarH1.innerText = 'Paleta de Cores';
-criarH1.id = 'title';
-const elementPrinc = document.body;
-elementPrinc.appendChild(criarH1);
+// for (let index = 0; index < 5; index += 1); {
+//     const pixels = document.querySelector('div')
+// }
+const corpo = document.querySelector('header');
+const crSection = document.createElement('section');
+crSection.id = 'pixel-board';
+
+corpo.appendChild(crSection);
+
+function makeDiv () {
+ const newDiv = document.createElement('div');
+ newDiv.className = 'pixel';
+ crSection.appendChild(newDiv)
+}
+function makeDivLine () {
+    const newDiv = document.createElement('div');
+    crSection.appendChild(newDiv)
+   }
+
+for (let i = 1; i <= 5; i += 1) {
+    makeDivLine();
+for (let index = 1; index <= 5; index += 1) {
+  makeDiv();
+}
+}
